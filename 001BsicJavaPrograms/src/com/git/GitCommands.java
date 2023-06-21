1627e09ba4ca04593f31a50e1a730aa8913e278d
SrimanG799 <srimaniq799@email.com>
2023-06-21 08:07:16 +0530

    hello new branch

---
+++ 001BsicJavaPrograms/src/com/git/GitCommands.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class GitCommands {
+	public static void main(String[] args) {
+		System.out.println("welcome");
+	}
+//git add  is used stage the changes
+	//git add --all it is save the all changes in all files at a time
+	//git commit is used to save the changes in local repository
+	//git commit -m "commit message"
+	//git push origin master/main is saves the changes in git Repository
+}
